bd39f041867551c5d5741e1445315a66c13ff22a
Amanda Guimarães Barbosa <amandaguimab@gmail.com>
2024-04-23 09:50:34 -0300

    A

---
+++ rz-midias/germano/estado.php
@@ -0,0 +1,26 @@
+<?php
+
+//  Initiate curl
+$url = "https://impostometro.com.br/Contador/Estado?estado=sc&ano=".date('Y', time());
+
+$ch = curl_init();
+// Will return the response, if false it print the response
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// Set the url
+curl_setopt($ch, CURLOPT_URL,$url);
+
+$headers = [
+    'Accept: application/json',
+    'X-Requested-With: XMLHttpRequest'
+];
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// Execute
+$result=curl_exec($ch);
+// Closing
+
+echo curl_error($ch);
+curl_close($ch);
+
+// Will dump a beauty json :3
+echo $result;
